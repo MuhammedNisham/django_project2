90dd9cb1e27e973d20d83a57f60be311b80b8f2b
MuhammedNisham <nishamovingal602@gmail.com>
2025-03-21 08:56:37 +0530

    product model created

--- Nshop/mainapp/models.py
+++ Nshop/mainapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# every class defined here
+
+
+class Product(models.Model):
+    #id will be automatically created by django
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    desc = models.TextField(max_length=500)
+    img = models.ImageField(upload_to='products/')
+    stock = models.PositiveIntegerField()
+
