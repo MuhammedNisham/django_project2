70dafd3ade5477737c25314fd3cee6891244ca78
MuhammedNisham <nishamovingal602@gmail.com>
2025-03-21 09:01:45 +0530

    product model created

--- Nshop/mainapp/admin.py
+++ Nshop/mainapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+#importing neccessary models
+
+from .models import Product
+
 # Register your models here.
+# Every model registered here, will  be available in django prebuilt admin panel
+admin.site.register(Product)
