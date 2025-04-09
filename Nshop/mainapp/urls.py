3f5f8309b0269bb5fde17bdda948eebabc8391a7
MuhammedNisham <nishamovingal602@gmail.com>
2025-04-09 08:41:14 +0530

    login page added

--- Nshop/mainapp/urls.py
+++ Nshop/mainapp/urls.py
@@ -4,5 +4,10 @@ from .import views
 urlpatterns =[
     path('', views.homeView, name = 'homepage'),
     path('about', views.aboutView, name = 'aboutpage'),
-    path('contact', views.ContactView, name = 'contactpage')
+    path('contact', views.ContactView, name = 'contactpage'),
+    path('products/<int:pk>', views.ProductDetails.as_view(), name = 'prod_details'),
+    path('products/<int:pk>', views.ProductDetails.as_view(), name = 'prod_details'),
+    path('products/add', views.AddProduct.as_view(), name='add_product'),
+    path('products/edit/<int:pk>', views.EditProduct.as_view(), name='edit_product'),
+    path('products/del/<int:pk>', views.DelProduct.as_view(), name='del_product')
 ]
--- Nshop/mainapp/views.py
+++ Nshop/mainapp/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.http import HttpResponse
 
 from django.views.generic import DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Product
 # Create your views here.
@@ -35,3 +36,23 @@ def ContactView(request):
     template = loader.get_template('contact.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+class AddProduct(CreateView):
+     model = Product
+     template_name = 'add_products.html'
+     fields = '__all__' # include all the fields in the form
+     # This view on successful addition of products, redirects the user to a 
+     # new page, to set this page, we do the below line of code
+     success_url = '/'
+
+class EditProduct(UpdateView):
+     model = Product
+     context_object_name = 'product'
+     template_name = 'edit_product.html'
+     fields = ['img','price', 'stock', 'desc']
+     success_url = '/'
+
+class DelProduct(DeleteView):
+     model = Product
+     template_name = 'del_product.html'
+     success_url = '/'
