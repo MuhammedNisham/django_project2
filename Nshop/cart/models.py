f5119ef88c7b0b6ae1fc0e9f0b4909f001ad8729
MuhammedNisham <nishamovingal602@gmail.com>
2025-04-17 08:18:57 +0530

    edits

--- Nshop/cart/models.py
+++ Nshop/cart/models.py
@@ -6,14 +6,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class CartItem(models.Model):
-    product = models.ForeignKey(Product, on_delete = models.CASCADE)
 
-    user = models.ForeignKey(User, on_delete= models.CASCADE)
-
-    quantity = models.IntegerField(default=0)
+    product = models.ForeignKey(Product, on_delete = models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null=True)
+    quantity = models.IntegerField(default=0) # INTEGER
+    date_added = models.DateTimeField(auto_now_add=True) # Current timestamp
 
     date_added = models.DateTimeField(auto_now_add=True)
     #for current time stamp
 
     def __str__(self):
         return f"{self.product.name} added by {self.user.username} at {self.date_added}"
+    
+    def get_total(self):
+         return self.product.price * self.quantity
--- Nshop/cart/views.py
+++ Nshop/cart/views.py
@@ -2,21 +2,27 @@ from django.shortcuts import render, redirect
 from django.template import loader
 from .models import CartItem
 from mainapp.models import Product
+from django.contrib.auth.decorators import login_required
  
  
  # Create your views here
- 
+
+@login_required
 def viewCart(request):
     cartItems = CartItem.objects.filter(user = request.user)
+
+    total_price = sum([float(item.product.price) * item.quantity for item in cartItems])
  
     template = 'cart.html'
  
     context = {
-       'items' : cartItems
+       'items' : cartItems,
+       'total' : total_price
     }
  
     return render(request, template, context)
 
+@login_required
 def addToCart(request, product_id):
 
     this_product = Product.objects.get(id = product_id)
@@ -24,4 +30,11 @@ def addToCart(request, product_id):
     cart_item.quantity += 1
 
     cart_item.save()
+    return redirect('view_cart')
+
+@login_required
+def remFromCart(request,cart_item_id):
+    this_cart_item = CartItem.objects.get(id = cart_item_id)
+    this_cart_item.delete() # this will delete the cart_item_object and its associated record in the CartItem table in db
+ 
     return redirect('view_cart')
